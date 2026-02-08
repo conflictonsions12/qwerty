dc08928efc775d356fb467737801d05ff920cbdb
conflictonsions12 <conflictonsions12@gmail.com>
2026-02-08 15:24:45 -0600

    Update aboutData.js

--- src/constants/aboutData.js
+++ src/constants/aboutData.js
@@ -1,7 +1,7 @@
 // src/constants/aboutData.js
 export const aboutData = {
   label: "ABOUT US",
-  title: "Dedicated Expertise in Agricultural Consulting",
+  title: "Dedicated expertise in agricultural consulting",
   description:
     "Since 2009, Dhorne agro consult has stood as a cornerstone in the agricultural industry, offering specialized services in crop planning, soil testing, and customized agricultural strategies. With a dedication to excellence and innovation, we provide reliable solutions that cater to individual farm needs, helping farmers enhance productivity and sustainability. Our approach combines decades of expertise with advanced agricultural practices to ensure that clients receive the most effective and efficient advice for their farming operations.",
   image: "/about-farmers.jpg",
