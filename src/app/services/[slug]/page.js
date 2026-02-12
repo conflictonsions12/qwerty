7bd0b7bbf666e405953508910ee14eba3ab5e772
conflictonsions12 <conflictonsions12@gmail.com>
2026-02-12 01:41:43 +0100

    Update page.js

--- src/app/services/[slug]/page.js
+++ src/app/services/[slug]/page.js
@@ -3,14 +3,16 @@ import Link from "next/link";
 import { services } from "@/constants/services";
 import { notFound } from "next/navigation";
 
+/*
 export async function generateStaticParams() {
   return services.map((service) => ({
     slug: service.slug,
   }));
 }
+*/
 
 export async function generateMetadata({ params }) {
-  const { slug } = await params;
+  const { slug } = params; // removed incorrect await
   const service = services.find((s) => s.slug === slug);
 
   if (!service) {
@@ -26,7 +28,7 @@ export async function generateMetadata({ params }) {
 }
 
 export default async function ServiceDetailPage({ params }) {
-  const { slug } = await params;
+  const { slug } = params; // removed incorrect await
   const service = services.find((s) => s.slug === slug);
 
   if (!service) {
