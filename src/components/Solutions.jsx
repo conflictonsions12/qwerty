54dc02f5c623991fac8a5e1264f7bb6de35804f6
conflictonsions12 <conflictonsions12@gmail.com>
2026-02-12 01:48:40 +0100

    Update Solutions.jsx

--- src/components/Solutions.jsx
+++ src/components/Solutions.jsx
@@ -57,7 +57,7 @@ export default function Solutions({ solutions }) {
         </motion.h2>
 
         {/* Cards Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-14">
           {solutions.map((solution, index) => (
             <motion.div
               key={solution.id}
